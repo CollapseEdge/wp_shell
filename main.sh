a737cd509f484d6eda12e27aed6f7f8cea66347a
CollapseEdge <123354572+CollapseEdge@users.noreply.github.com>
2023-03-29 23:52:58 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -25,7 +25,7 @@ case $result in
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
-        sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo apt-get install -y docker.io
         apt install docker-compose
 		wget https://raw.githubusercontent.com/CollapseEdge/wp_shell/main/docker-compose.yml
         systemctl start docker.service
