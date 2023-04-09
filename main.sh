cf23b9a41e659b3bc4076430410bb61f4e0dee6d
CollapseEdge <123354572+CollapseEdge@users.noreply.github.com>
2023-04-09 22:01:52 +0800

    修复docker安装的wordpress无法上传大体积文件的问题

--- main.sh
+++ main.sh
@@ -21,23 +21,18 @@ echo -n "确认按照(y|n):"
 read result
 case $result in
 	[yY][eE][sS]|[yY])
-		echo "安装docker"
+	echo "安装docker"
         sudo apt-get update
-		sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt-get update
-        sudo apt-get install -y docker.io
-        apt install docker-compose
-		wget https://raw.githubusercontent.com/CollapseEdge/wp_shell/main/docker-compose.yml
+	curl -sSL https://get.docker.com/ | sh
         systemctl start docker.service
         systemctl enable docker
-        docker-compose -f docker-compose.yml up -d
-	rm -f docker-compose.yml
-		echo -e "\033[32m ---安装成功--- \033[0m" 
-		echo "请访问 http://`curl -s ident.me`/wp-admin/install.php"
-		echo "并按照指引完成安装"
-		echo "如果访问之后出现安装错误的提示,请强制刷新网页"
+	wget https://raw.githubusercontent.com/CollapseEdge/wp_shell/main/upload.ini
+        docker run --name wordpress --link db:mysql -p 32768:80 -e WORDPRESS_DB_HOST=mysql:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=root -e WORDPRESS_DB_NAME=wordpress -e WORDPRESS_TABLE_PREFIX=wp_ -v /home/wordpress/upload.ini:/usr/local/etc/php/conf.d/uploads.ini -d wordpress
+        rm -f upload.ini
+	echo -e "\033[32m ---安装成功--- \033[0m" 
+	echo "请访问 http://`curl -s ident.me`/wp-admin/install.php"
+	echo "并按照指引完成安装"
+	echo "如果访问之后出现安装错误的提示,请强制刷新网页"
 		;;
 	[nN][oO]|[nN])
 		echo "no"
